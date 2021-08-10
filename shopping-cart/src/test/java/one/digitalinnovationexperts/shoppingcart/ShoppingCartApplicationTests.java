f6d81d30c8cf63ecacc15a687f5d82be2ce6e2fd
maurilioSousa-Desing <fmaurilio6@gmail.com>
2021-08-10 18:53:09 -0300

    Create ShoppingCartApplicationTests.java

---
+++ shopping-cart/src/test/java/one/digitalinnovationexperts/shoppingcart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovationexperts.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
